ab2db1e857c1e616b54dfefd323d653c2726e528
Sergey Grinko <sg2813716@gmail.com>
2023-11-01 22:08:16 +0200

    finished html & css of  layout

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     slidesPerView: 3,
     direction: 'horizontal',
     loop: true,
     spaceBetween:16,
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
